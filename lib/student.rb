03bf4a483ebccd42968070762b022c40b6eb1e0e
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-24 09:58:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,11 +19,14 @@ class Student
         grade INTEGER
       )
       SQL
-      DB[:conn].execute(sql)
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
-
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+    DB[:conn].execute(sql)
   end
 
 end #class Student
