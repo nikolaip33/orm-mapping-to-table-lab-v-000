c29a20948977a968cac01466a30391337ccfc01c
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-24 10:07:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,12 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album) VALUES (?,?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+  end
+
 end #class Student
